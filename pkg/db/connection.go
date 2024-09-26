907c9c6d65ddf1f7d575d73118eef1eff48866fc
galera <galera@work>
2024-09-26 13:41:34 +0200

    feat: Database connection pool

---
+++ pkg/db/connection.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	// "fmt"
+	// "context"
+	// "io"
+	// "log"
+	// "net/http"
+	// "os"
+
+	// "github.com/jackc/pgx/v5"
+	// "github.com/jackc/pgx/v5/pgxpool"
+)
+
+func Hello() string {
+    return "Hello, world."
+}
